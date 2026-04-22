9a19ce59289375281a30f26152eaf54b8025c354
Nahu <nahucorbalan06@gmail.com>
2026-04-22 17:00:17 -0300

    7. Conexión de Menú a Generadores

--- scenes/Menu/Menu.gd
+++ scenes/Menu/Menu.gd
@@ -5,6 +5,8 @@ const INSTANCES_TEXT = "Cantidad de instancias: "
 var instancesCount = 0
 
 func _ready():
+	$RedGenerator.button_down.connect(count_new_instance)
+	$VioletGenerator.button_down.connect(count_new_instance)
 	# Inicializa el texto de "CountLabel" usando el texto
 	# Constante "INSTANCES_TEXT" más el valor de "instancesCount" como Texto
 	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
